03a564c67d3b776ff010223072f4991ba33c349a
Mi-Ski <skibamichal@interia.pl>
2023-02-27 00:07:29 +0100

    websocket listening for a new post

--- index.js
+++ index.js
@@ -12,21 +12,28 @@ app.use(express.static(path.join(__dirname, "/public")));
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", function (ws) {
-  const id = setInterval(function () {
-    ws.send(JSON.stringify(process.memoryUsage()), function () {
-      //
-      // Ignoring errors.
-      //
-    });
-  }, 100);
-  console.log("started client interval");
+wss.on('connection', function connection(ws) {
+  console.log('Client connected');
 
-  ws.on("close", function () {
-    console.log("stopping client interval");
-    clearInterval(id);
+  ws.on('message', function incoming(message) {
+    console.log(`Received message: ${message}`);
+    // Parse incoming message as JSON
+    const post = JSON.parse(message);
+
+    // Broadcast new post to all connected clients
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(post));
+      }
+    });
+  });
+
+  ws.on('close', function close() {
+    console.log('Client disconnected');
   });
 });
+
+
 
 server.listen(8080, function () {
   console.log("Listening on http://0.0.0.0:8080");
